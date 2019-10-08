9a986a632ed9661d9748f1dfa721f9904896bcea
willskhalifa <50887729+Ashutoshwahane@users.noreply.github.com>
2019-10-08 12:22:37 +0530

    Delete skutest.py

--- skutest.py
+++
@@ -1,9 +0,0 @@
-def productsku(str1, dict, l_dict=""):
-    for i in dict:
-        l_dict += dict[i][:3] + "-"
-    return (str1[:3]+"-"+l_dict[:-1]).upper()
-
-
-str1 = "apple"
-dict = {"model": "iphone", "color": "red", "os": "ios13"}
-print(productsku(str1, dict, l_dict=""))
